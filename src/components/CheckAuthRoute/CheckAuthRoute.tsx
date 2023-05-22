b1f7fae354999cc4eb22f5a3ac8292ec016cf673
Alexey Vishnyakov <n3tn0de@gmail.com>
2023-05-22 20:21:39 +0400

    Add token storage

--- src/components/CheckAuthRoute/CheckAuthRoute.tsx
+++ src/components/CheckAuthRoute/CheckAuthRoute.tsx
@@ -4,6 +4,7 @@ import {
   Redirect,
 } from "react-router-dom";
 
+const token = localStorage.getItem('token')
 
 type Props = {
   path: string,
@@ -18,17 +19,23 @@ export const CheckAuthRoute = ({
   ...rest
 }: PropsWithChildren<Props>
 ) => {
-  let auth = true;
+  let hasAuth = (!token || token === null) ? false : true;
+  // in case token is present but invalid, we can invalidate it by implementting
+  // handler(s) for 403 API respone and also redirect user back to login
+  // But https://reqres.in/ doesn't have APIs that _require_ tokens,
+  // so I can't demo that, plus I'm short on time unfortunately
 
   if (reverse) {
-    auth = !auth
+    // this is for pages we want to redirect user away from if they're logged in
+    // like the login page itself
+    hasAuth = !hasAuth
   }
 
   return (
     <Route
       {...rest}
       render={({ location }) => {
-        if (auth) {
+        if (hasAuth) {
           return children
         }
         return(
